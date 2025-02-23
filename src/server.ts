d059a11562423454c9f71f5617118a434890be67
mpauloalves <marcospauloas17@gmail.com>
2025-02-23 16:49:19 -0300

    API desenvolvida para o projeto de convites

---
+++ src/server.ts
@@ -0,0 +1,48 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
+import {
+    type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { env } from './env'
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
+import { accessInviteLinkRoute } from './routes/access-invite-link-route'
+import { getSubscriberInviteClicksRoute } from './routes/get-subscriber-invite-clicks-route'
+import { getSubscriberInvitesCountRoute } from './routes/get-subscriber-invites-count-route'
+import { getSubscriberRankingPositionRoute } from './routes/get-subscriber-ranking-position-route'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '0.0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(subscribeToEventRoute)
+app.register(accessInviteLinkRoute) 
+app.register(getSubscriberInviteClicksRoute)
+app.register(getSubscriberInvitesCountRoute)
+app.register(getSubscriberRankingPositionRoute)
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log('HTTP server running!')
+})
